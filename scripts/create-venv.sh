ee0a77907d1a647ab7792c5b3cf25b37b4a7c1ff
Michael Hansen <michael.hansen.24@us.af.mil>
2020-11-30 15:49:05 -0500

    Add Larynx as a submodule

--- scripts/create-venv.sh
+++ scripts/create-venv.sh
@@ -13,26 +13,12 @@ src_dir="$(realpath "${this_dir}/..")"
 
 venv="${src_dir}/.venv"
 download="${src_dir}/download"
+larynx_dir="${src_dir}/larynx"
 
-# -----------------------------------------------------------------------------
-
-function maybe_download {
-    if [[ ! -s "$2" ]]; then
-        mkdir -p "$(dirname "$2")"
-        curl -sSfL -o "$2" "$1" || { echo "Can't download $1"; exit 1; }
-        echo "$1 => $2"
-    fi
-}
-
-larynx_file="${download}/larynx-0.2.0.tar.gz"
-larynx_url='https://github.com/rhasspy/larynx/archive/v0.2.0.tar.gz'
-
-maybe_download "${larynx_url}" "${larynx_file}"
-
-tts_file="${download}/TTS-0.2.0.tar.gz"
-tts_url='https://github.com/rhasspy/TTS/archive/v0.2.0.tar.gz'
-
-maybe_download "${tts_url}" "${tts_file}"
+if [[ ! -d "${larynx_dir}/TTS/TTS" ]]; then
+    echo "Larynx not found. Try running 'git submodule update --init --recursive'"
+    exit 1
+fi
 
 # -----------------------------------------------------------------------------
 
@@ -57,19 +43,12 @@ pip3 ${PIP_INSTALL} -r requirements.txt
 
 # Install Larynx
 echo 'Installing Larynx'
-larynx_dir="${src_dir}/larynx"
-rm -rf "${larynx_dir}"
-mkdir -p "${larynx_dir}"
-tar -C "${larynx_dir}" --strip-components=1 -xf "${larynx_file}"
 
 # Turn it into a fake Python module
 touch "${larynx_dir}/__init__.py"
 
 # Add MozillaTTS fork as a submodule
 tts_dir="${larynx_dir}/TTS"
-rm -rf "${tts_dir}"
-mkdir -p "${tts_dir}"
-tar -C "${tts_dir}" --strip-components=1 -xf "${tts_file}"
 
 # Install MozillaTTS fork dependencies
 pushd "${tts_dir}"
